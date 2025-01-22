d7abf3379b4487e2cf70cb485f70c92e9cec0dde
Adrian Escutia Soto <adrianescutia@yahoo.com.mx>
2025-01-22 05:20:18 -0600

    feat: new client integration w/MCP SEE, remove environment vars & update .gitignore;

--- src/components/Tools/Helpers.ts
+++ src/components/Tools/Helpers.ts
@@ -1,13 +1,28 @@
 import tools from '../../data/tools.json';
-import { createOrUpdateFile } from '@modelcontextprotocol/server-github/dist/operations/files';
+import { Client } from "@modelcontextprotocol/sdk/client/index.js";
+import { SSEClientTransport } from "@modelcontextprotocol/sdk/client/sse.js";
+
+import packageJson from "../../../package.json";
 
 const repoDetails = {
   owner: 'agentico-dev',
   repo: 'website',
   src: 'src/components/ToolsCatalog/tools.json',
-  branch: 'deleteme'
+  branch: 'deleteme',
+  message: 'Add new tool'
 };
 
+const client = new Client({
+  name: "agentico-dev-website",
+  description: packageJson.description,
+  version: packageJson.version
+}, {
+  capabilities: {}
+});
+
+const transport = new SSEClientTransport(
+  new URL("http://localhost:3000/sse")
+);
 export const handleToolSubmit = async (newTool: any) => {
   try {
     let updatedTools = {};
@@ -28,16 +43,14 @@ export const handleToolSubmit = async (newTool: any) => {
       };
     }
     console.log(`Adding tool to https://api.github.com/repos/${repoDetails.owner}/${repoDetails.repo}/contents/${repoDetails.src}`);
-    const result = await createOrUpdateFile(
-      repoDetails.owner,
-      repoDetails.repo,
-      repoDetails.src,
-      // @todo - validate that the content doesn't already exist
-      JSON.stringify(updatedTools, null, 2),
-      'Add new tool',
-      repoDetails.branch
-    );
+    // Call a tool
+    await client.connect(transport);
+    const result = await client.callTool({
+      name: "create_or_update_file",
+      arguments: { ...repoDetails, content: JSON.stringify(updatedTools, null, 2) }
+    });
     console.log('Tool added successfully:', result);
+    client.close();
     return result;
   } catch (error) {
     console.error('Error updating tools.json:', error);
